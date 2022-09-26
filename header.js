c76252949d3695b0298694c5f78e5929e3f59914
Jourdan Neal <jjjet1991@gmail.com>
2022-09-25 22:21:28 -0700

    updating fonts

--- header.js
+++ header.js
@@ -17,10 +17,10 @@ connectedCallback(){
     <div>
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#829460;margin-bottom: 0;">
             <div class="container">
-              <a class="navbar-brand" href="./index.html">Home</a>
-              <a class="navbar-brand" href="./about.html">About</a>
-              <a class="navbar-brand" href="./portfolio.html">Portfolio</a>
-              <a class="navbar-brand" href="./contact.html">Contact</a>
+              <a class="navbar-brand"  style="font-family: 'Allerta Stencil', sans-serif;" href="./index.html">Home</a>
+              <a class="navbar-brand"  style="font-family: 'Allerta Stencil', sans-serif;" href="./about.html">About</a>
+              <a class="navbar-brand"  style="font-family: 'Allerta Stencil', sans-serif;" href="./portfolio.html">Portfolio</a>
+              <a class="navbar-brand"  style="font-family: 'Allerta Stencil', sans-serif;" href="./contact.html">Contact</a>
             </div>
           </nav>
         </div>    
