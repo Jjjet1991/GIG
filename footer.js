0083fe0e8704afe27453595b8ecb57f675045d00
Jourdan Neal <jjjet1991@gmail.com>
2022-10-03 03:31:14 -0700

    updating portfolio page-error to fix

--- footer.js
+++ footer.js
@@ -6,12 +6,12 @@ class Footer extends HTMLElement
 
 connectedCallback(){
     this.innerHTML = `
-    <div  style="background-color:#829460;height:70px;margin-top: 10px;">
+    <div  style="background-color:#829460;height:70px;">
     <div class="" style="margin-left:45%;">
         <a href="#" class="fa fa-facebook"></a>
         <a href="#" class="fa fa-instagram"></a>
         <a href="#" class="fa fa-pinterest"></a>
-        <a href="https://www.etsy.com/shop/GIGWoodcraft?ref=shop_sugg" class="fa fa-etsy"></a>
+        <a href="https://www.etsy.com/shop/GIGWoodcraft?ref=shop_sugg" target="blank" class="fa fa-etsy"></a>
     </div>
 </div>`
  }
